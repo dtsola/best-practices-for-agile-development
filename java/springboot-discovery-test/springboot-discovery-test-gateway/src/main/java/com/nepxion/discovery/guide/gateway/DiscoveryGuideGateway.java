e0d0e2f19c677dc305319709bb542d0d8fdc9df2
dtsola <dtsola@163.com>
2022-09-20 18:15:00 +0800

    添加协议以及说明

--- java/springboot-discovery-test/springboot-discovery-test-gateway/src/main/java/com/nepxion/discovery/guide/gateway/DiscoveryGuideGateway.java
+++ java/springboot-discovery-test/springboot-discovery-test-gateway/src/main/java/com/nepxion/discovery/guide/gateway/DiscoveryGuideGateway.java
@@ -1,13 +1,19 @@
-package com.nepxion.discovery.guide.gateway;
-
-/**
- * <p>Title: Nepxion Discovery</p>
- * <p>Description: Nepxion Discovery</p>
- * <p>Copyright: Copyright (c) 2017-2050</p>
- * <p>Company: Nepxion</p>
- * @author Haojun Ren
- * @version 1.0
+/*
+ * Copyright 2022 Dtsola.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+package com.nepxion.discovery.guide.gateway;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
